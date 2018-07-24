104c20f3165f415487dd79a3ca306b55b445fa6f
Himashi Hettege Dona <himashi99@hotmail.com>
2018-07-24 18:59:25 -0400

    Add parallax effect

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,25 +1,49 @@
-// var ypos;
-// var image;
-// function parallax1() {
-//   ypos = window.pageYOffset;
-//   image = document.getElementById("description");
-//   image.style.top = ypos * 0.2 + 'px';
-// }
-// window.addEventListener('scroll', parallax1);
-// 
-// function parallax2() {
-//   ypos = window.pageYOffset;
-//   image = document.getElementById("background1");
-//   image.style.top = ypos * 0.2 + 'px';
-// }
-// window.addEventListener('scroll', parallax2);
+var ypos;
+var image1;
+function parallax1() {
+  ypos = window.pageYOffset;
+  image1 = document.getElementById("background1");
+  image1.style.top = ypos * -0.2 + 'px';
+}
+window.addEventListener('scroll', parallax1);
 
-// function parallax3() {
-//   ypos = window.pageYOffset;
-//   image = document.getElementById("about-us");
-//   image.style.top = ypos * 0.2 + 'px';
+var image2;
+function parallax2() {
+  ypos = window.pageYOffset;
+  image2 = document.getElementById("about-us");
+  image2.style.top = ypos * -0.4 + 'px';
+}
+window.addEventListener('scroll', parallax2);
+
+
+function parallax3() {
+  var image3;
+  var d = document.getElementById("background2");
+  var topPos = d.offsetTop;
+
+  image3 = document.getElementById("background2");
+  image3.style.top = topPos * -0.5 + 'px';
+}
+window.addEventListener('scroll', parallax3);
+
+// function parallax4() {
+//   var image4;
+//   var e = document.getElementById("gallery-scroll");
+//   var topPos2 = e.offsetTop;
+//
+//   image4 = document.getElementById("gallery-scroll");
+//   image4.style.top = topPos2 * -0.1 + 'px';
 // }
-// window.addEventListener('scroll', parallax3);
+// window.addEventListener('scroll', parallax4);
+
+var image4;
+function parallax4() {
+  ypos = window.pageYOffset;
+  image4 = document.getElementById("gallery-scroll");
+  image4.style.top = ypos * -0.5 + 'px';
+}
+window.addEventListener('scroll', parallax4);
+
 
 $(function() {
   $('#yoyo').hover(function() {
